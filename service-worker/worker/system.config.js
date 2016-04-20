ad7c4070f777692e9850c58e43e5d894cec5707b
Jeff Cross <middlefloor@gmail.com>
2016-04-20 16:11:03 -0700

    wit emit type declarations with worker

--- service-worker/worker/system.config.js
+++ service-worker/worker/system.config.js
@@ -1,8 +1,12 @@
 System.config({
-	"transpiler": "typescript",
+	"transpiler": "ts",
 	"typescriptOptions": {
 		"emitDecoratorMetadata": true,
 		"experimentalDecorators": true,
+		"declaration": true
+	},
+	"paths": {
+		"ts": "node_modules/plugin-typescript/lib/",
 	},
 	"map": {
 		"typescript": "node_modules/typescript/lib/typescript.js",
@@ -12,10 +16,19 @@ System.config({
 	},
 	"packages": {
 		"app": {
-			"defaultExtension": "ts"
-		},
+      "defaultExtension": "ts",
+      "meta": {
+        "*.ts": {
+          "loader": "ts"
+        }
+      }
+    },
 		"angular2": {
 			"defaultExtension": "js"
+		},
+		"ts": {
+			"defaultExtension": "js",
+			"main": "plugin.js"
 		},
 		"reflect-metadata": {
 			"format": "global"
